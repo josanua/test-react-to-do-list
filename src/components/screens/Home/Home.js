2880ac1f6218d5cc7bd726c24f177bfcdcc93d33
Andrew <designwebmoldova@gmail.com>
2023-05-10 20:51:08 +0300

    Clean package.json and add reac-icons library

--- src/components/screens/Home/Home.js
+++ src/components/screens/Home/Home.js
@@ -1,24 +1,32 @@
 import React from 'react'
+import TodoItem from "./TodoItem";
 
 
 const todosData = [
     {
         title: 'Todo Title',
         isCompleted: false,
+        todoID: '_todo1'
     },
     {
         title: 'Todo Title 2',
         isCompleted: true,
+        todoID: '_todo2'
     },
     {
         title: 'Todo Title 3 biggest to se what will happens' ,
         isCompleted: false,
+        todoID: '_todo3'
     }
 ]
 const Home = () => {
     return (
         <div className='bg-gray-900 h-screen text-white'>
-            Home
+            {
+                todosData.map(todoItem => (
+                    <TodoItem todoData={todoItem} key={todoItem.todoID} />
+                ))
+            }
         </div>
     )
 }
---
+++ src/components/screens/Home/TodoItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TodoItem = ({todoData}) => {
+    return (
+            <div>{todoData.title}</div>
+    )
+}
+
+export default TodoItem
