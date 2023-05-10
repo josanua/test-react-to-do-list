7b5a122f9800d914597294c5fe93dbabd6434316
Andrew <designwebmoldova@gmail.com>
2023-05-10 20:26:07 +0300

    Add some demo data for to-do-list

--- src/components/screens/Home/Home.js
+++ src/components/screens/Home/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+
+const todosData = [
+    {
+        title: 'Todo Title',
+        isCompleted: false,
+    },
+    {
+        title: 'Todo Title 2',
+        isCompleted: true,
+    },
+    {
+        title: 'Todo Title 3 biggest to se what will happens' ,
+        isCompleted: false,
+    }
+]
 const Home = () => {
     return (
-        <div>
-
+        <div className='bg-gray-900 h-screen text-white'>
+            Home
         </div>
     )
 }
+
+export default Home
