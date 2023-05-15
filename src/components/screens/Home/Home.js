ceb50833733182a0c4cfe89255c8a8e00afbf720
Andrew <designwebmoldova@gmail.com>
2023-05-15 11:55:53 +0300

    Add addTodo func and test func

--- src/components/screens/Home/Home.js
+++ src/components/screens/Home/Home.js
@@ -17,6 +17,19 @@ const Home = () => {
         setTodos([...todos].filter(n => n.todoID !== id))
     }
 
+    const addTodo = (title) => {
+        setTodos([
+            {
+                todoID: new Date(), // to lazy to create func for Id iteration, just created a random id
+                title,
+                isCompleted: false
+            },
+            ...todos])
+    }
+
+    // For testing purposes
+    // window.addTodo = addTodo;
+
     return (
         <div className='text-white w-4/5 mx-auto'>
             <h1 className="text-2xl font-bold text-center mb-3">My first to-do list</h1>
--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 // let data to be here
 const data = [
     {
+        todoID: '_todo1',
         title: 'Todo Title',
         isCompleted: false,
-        todoID: '_todo1'
     },
     {
+        todoID: '_todo2',
         title: 'Todo Title 2',
         isCompleted: false,
-        todoID: '_todo2'
     },
     {
+        todoID: '_todo3',
         title: 'Todo Title 3 biggest to se what will happens',
         isCompleted: false,
-        todoID: '_todo3'
     }
 ]
 export default data;
