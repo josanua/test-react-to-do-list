f1b37ae4433b9f68cc251dff0f3d3b817897e348
Andrew <designwebmoldova@gmail.com>
2023-05-15 15:32:13 +0300

    Finish with layout design of CreateTodoFileds component

--- src/components/screens/Home/create-todo-field/CreateTodoField.js
+++ src/components/screens/Home/create-todo-field/CreateTodoField.js
@@ -3,15 +3,16 @@ import {useState} from "react";
 const CreateTodoField = ({addTodo}) => {
     const [title, setTitle] = useState('')
 
-    console.log(title);
-
     return (
-        <div className="flex items-center justify-between mb-4 rounded-2xl bg-zinc-800 p-5 w-full">
+        <div className="flex items-center justify-between mb-4 rounded-2xl
+        border-zinc-800 border-2 px-5 py-3 w-full mt-10">
             <input
                 type="text"
                 onChange={e => setTitle(e.target.value)}
                 value={title}
                 onKeyDown={e => e.key === 'Enter' && addTodo(title)}
+                className="bg-transparent w-full border-none outline-none"
+                placeholder="Add a task"
             />
         </div>
     )
