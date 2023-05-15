784defa341e59cc46102c333cc547639c8f652c4
Andrew <designwebmoldova@gmail.com>
2023-05-15 15:23:19 +0300

    Create input controller

--- src/components/screens/Home/create-todo-field/CreateTodoField.js
+++ src/components/screens/Home/create-todo-field/CreateTodoField.js
@@ -1,6 +1,18 @@
-const CreateTodoField = () => {
+import {useState} from "react";
+
+const CreateTodoField = ({addTodo}) => {
+    const [title, setTitle] = useState('')
+
+    console.log(title);
+
     return (
-        <div>
+        <div className="flex items-center justify-between mb-4 rounded-2xl bg-zinc-800 p-5 w-full">
+            <input
+                type="text"
+                onChange={e => setTitle(e.target.value)}
+                value={title}
+                onKeyPress={e => e.key === 'Enter' && addTodo(title)}
+            />
         </div>
     )
 }
