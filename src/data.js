6194acf8cec9a4d119e176f00b724e4ae586f354
Andrew <designwebmoldova@gmail.com>
2023-06-06 16:37:45 +0300

    Change demo data

--- src/data.js
+++ src/data.js
@@ -2,18 +2,18 @@
 const data = [
     {
         todoID: '_todo1',
-        title: 'Todo Title Zina',
+        title: 'First todo demo item',
         isCompleted: false,
     },
-    {
-        todoID: '_todo2',
-        title: 'Todo Title 2',
-        isCompleted: false,
-    },
-    {
-        todoID: '_todo3',
-        title: 'Todo Title 3 biggest to se what will happens',
-        isCompleted: false,
-    }
+    // {
+    //     todoID: '_todo2',
+    //     title: 'Todo Title 2',
+    //     isCompleted: false,
+    // },
+    // {
+    //     todoID: '_todo3',
+    //     title: 'Todo Title 3 biggest to se what will happens',
+    //     isCompleted: false,
+    // }
 ]
 export default data;
